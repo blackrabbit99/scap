19d8f51e4497900a8b545cb7dc2014774e9f1970
blackrabbit <myzlio@gmail.com>
2013-02-03 21:21:50 +0200

    simple architect

---
+++ samples/demo/demo/js/sandbox.js
@@ -0,0 +1,28 @@
+var Sandbox =  {
+    create : function (core, module_selector) {
+        var CONTAINER = core.dom.query('#' + module_selector);
+        return {
+            find : function (selector) {
+                 
+            },
+            addEvent : function (element, type, fn) {
+                          
+            },
+            removeEvent : function (element, type, fn) {
+                             
+            },
+            notify : function (evt) {
+                    
+            },
+            listen : function (evts) {
+                
+            },
+            ignore : function (evts) {
+                 
+            },
+            create_element : function (el, config) {
+                 
+            }
+        };
+    }
+}
